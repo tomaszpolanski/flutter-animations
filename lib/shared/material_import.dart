2fb06830881358093ac028aca04865bb6713be86
Tomek Polanski <polanski.tomek@gmail.com>
2019-07-23 20:27:24 +0200

    Creating animated header

--- lib/shared/material_import.dart
+++ lib/shared/material_import.dart
@@ -1,9 +1,17 @@
-// For web
-export 'package:flutter_web/material.dart';
-export 'package:flutter_web_ui/ui.dart' show webOnlyInitializePlatform;
+//// For web
+//export 'package:flutter_web/material.dart';
+//export 'package:flutter_web_ui/ui.dart' show webOnlyInitializePlatform;
+//export 'dart:html';
 
-////For native
-//export 'package:flutter/foundation.dart';
-//export 'package:flutter/material.dart';
-//
-//Future<void> webOnlyInitializePlatform() async {}
+//For native
+export 'package:flutter/foundation.dart';
+export 'package:flutter/material.dart';
+export 'package:flutter/services.dart';
+
+Future<void> webOnlyInitializePlatform() async {}
+
+final Window window = Window();
+
+class Window {
+  void open(String url, String name) {}
+}
