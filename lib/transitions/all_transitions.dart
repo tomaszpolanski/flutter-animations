e359f5c1df4f7dc0b7fde35b572a02d11aab3c58
Tomek Polanski <tpolanski@groupon.com>
2019-07-24 07:51:46 +0200

    Adding transitions

--- lib/transitions/all_transitions.dart
+++ lib/transitions/all_transitions.dart
@@ -1,6 +1,10 @@
 import 'package:animation_cheat_page/shared/material_import.dart';
 import 'package:animation_cheat_page/shared/network/urls.dart';
+import 'package:animation_cheat_page/transitions/fade.dart';
+import 'package:animation_cheat_page/transitions/positioned.dart';
+import 'package:animation_cheat_page/transitions/rotation.dart';
 import 'package:animation_cheat_page/transitions/scale.dart';
+import 'package:animation_cheat_page/transitions/size.dart';
 import 'package:animation_cheat_page/transitions/slide.dart';
 
 final allTransitions = [
@@ -18,6 +22,42 @@ final allTransitions = [
     body: 'Scale transition changes the size of the widget.',
     url: '$rootUrl/blob/master/lib/transitions/scale.dart',
     builder: (animation, child) => ScaleExample(
+      animation: animation,
+      child: child,
+    ),
+  ),
+  Example(
+    title: 'RotationTransition',
+    body: 'TODO: pass the number of rotation',
+    url: '$rootUrl/blob/master/lib/transitions/rotation.dart',
+    builder: (animation, child) => RotationExample(
+      animation: animation,
+      child: child,
+    ),
+  ),
+  Example(
+    title: 'SizeTransition',
+    body: 'TODO: difference between size and scale',
+    url: '$rootUrl/blob/master/lib/transitions/size.dart',
+    builder: (animation, child) => SizeExample(
+      animation: animation,
+      child: child,
+    ),
+  ),
+  Example(
+    title: 'FadeTransition',
+    body: 'TODO',
+    url: '$rootUrl/blob/master/lib/transitions/fade.dart',
+    builder: (animation, child) => FadeExample(
+      animation: animation,
+      child: child,
+    ),
+  ),
+  Example(
+    title: 'PositionedTransition',
+    body: 'TODO',
+    url: '$rootUrl/blob/master/lib/transitions/positioned.dart',
+    builder: (animation, child) => PositionedExample(
       animation: animation,
       child: child,
     ),
