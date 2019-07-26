01deef55c49148a69e4707508d3eb5c7a674d2c6
Tomek Polanski <polanski.tomek@gmail.com>
2019-07-26 19:19:17 +0200

    Better code

--- lib/shared/code/code_button.dart
+++ lib/shared/code/code_button.dart
@@ -1,7 +1,7 @@
 import 'package:animation_cheat_page/shared/material_import.dart';
 import 'package:http/http.dart' as http;
 
-class CodeButton extends StatelessWidget {
+class CodeButton extends StatefulWidget {
   const CodeButton({
     Key key,
     @required this.url,
@@ -14,16 +14,33 @@ class CodeButton extends StatelessWidget {
   final ValueChanged<String> onFetched;
 
   @override
+  _CodeButtonState createState() => _CodeButtonState();
+}
+
+class _CodeButtonState extends State<CodeButton> {
+  bool _isLoading = false;
+
+  @override
   Widget build(BuildContext context) {
     return IconButton(
-      icon: Icon(Icons.code),
+      icon: _isLoading
+          ? const SizedBox(
+              height: 20,
+              width: 20,
+              child: CircularProgressIndicator(),
+            )
+          : Icon(Icons.code),
       tooltip: 'Show code',
       onPressed: () async {
-        final code = await http.get(url);
-        onFetched(
-          code.body.replaceAll(
-              "import 'package:animation_cheat_page/shared/material_import.dart';",
-              ''),
+        setState(() => _isLoading = true);
+        final code = await http.get(widget.url);
+        setState(() => _isLoading = false);
+        widget.onFetched(
+          code.body
+              .replaceAll(
+                  "import 'package:animation_cheat_page/shared/material_import.dart';",
+                  '')
+              .trim(),
         );
       },
     );
