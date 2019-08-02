447f3b38ff8f3336855df5a68bc9685fd2a4e938
Tomek Polański <polanski.tomek@gmail.com>
2019-08-02 07:14:49 +0200

    Positioned directional and opacity widgets (#42)
    #28 #29

---
+++ lib/animated_widgets/positioned_directional.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+
+import 'package:animation_cheat_page/shared/material_import.dart';
+
+class PositionedDirectionalExample extends StatefulWidget {
+  const PositionedDirectionalExample({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _PositionedDirectionalExampleState createState() =>
+      _PositionedDirectionalExampleState();
+}
+
+class _PositionedDirectionalExampleState
+    extends State<PositionedDirectionalExample> {
+  bool _showFirst = false;
+  Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    // starts animating just after the first frame
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) => setState(() => _showFirst = !_showFirst),
+    );
+    _timer = Timer.periodic(
+      const Duration(seconds: 4),
+      (_) => setState(() => _showFirst = !_showFirst),
+    );
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Stack(
+        children: [
+          AnimatedPositionedDirectional(
+            duration: const Duration(seconds: 4),
+            top: _showFirst ? 0 : 100,
+            start: _showFirst ? 0 : 100,
+            end: 0,
+            child: const Card(
+              color: Colors.yellowAccent,
+              child: Padding(
+                padding: EdgeInsets.all(40),
+                child: Icon(
+                  Icons.star,
+                  size: 50,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
