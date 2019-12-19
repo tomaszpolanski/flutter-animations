b9d0f67ffd35730923e77ea4263e9672f9688489
Tomek Polanski <tpolanski@groupon.com>
2019-12-19 11:40:40 +0100

    Table of content

---
+++ lib/shared/ui/search_bar.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class SearchBar extends StatefulWidget implements PreferredSizeWidget {
+  const SearchBar({
+    Key key,
+    @required this.onChanged,
+  }) : super(key: key);
+
+  final ValueChanged<String> onChanged;
+
+  @override
+  _SearchBarState createState() => _SearchBarState();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class _SearchBarState extends State<SearchBar> {
+  TextEditingController _controller;
+
+  @override
+  void initState() {
+    _controller = TextEditingController()
+      ..addListener(() {
+        widget.onChanged(_controller.text);
+      });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      title: TextField(
+        controller: _controller,
+        style: Theme.of(context).primaryTextTheme.title,
+        cursorColor: Theme.of(context).primaryTextTheme.title.color,
+        keyboardType: TextInputType.text,
+        autofocus: true,
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: _controller.clear,
+        )
+      ],
+    );
+  }
+}
