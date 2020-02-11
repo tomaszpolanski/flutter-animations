fd65208835224199874503b0f2b24d2f08513ba2
Tomek Polański <polanski.tomek@gmail.com>
2020-02-11 20:09:59 +0100

    Adding suggestions section (#96)
    * Adding suggestions section
    
    * WIP

---
+++ lib/shared/lists.dart
@@ -0,0 +1,19 @@
+extension IterableEx<T> on Iterable<T> {
+  Iterable<T> joinEx(T separator) sync* {
+    final iterator = this.iterator;
+    if (iterator.moveNext()) {
+      yield iterator.current;
+      while (iterator.moveNext()) {
+        yield separator;
+        yield iterator.current;
+      }
+    }
+  }
+
+  Iterable<R> mapIndexed<R>(R Function(int index, T) mapper) sync* {
+    int i = 0;
+    for (final value in this) {
+      yield mapper(i++, value);
+    }
+  }
+}
