38bd6fb2a6880c27c10c29d6d787c26ca5eb5c4f
Tomek Polanski <polanski.tomek@gmail.com>
2020-03-18 12:04:04 +0100

    Updating linux

---
+++ linux/window_configuration.cc
@@ -0,0 +1,5 @@
+#include "window_configuration.h"
+
+const char *kFlutterWindowTitle = "Flutter Animations";
+const unsigned int kFlutterWindowWidth = 500;
+const unsigned int kFlutterWindowHeight = 900;
