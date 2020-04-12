11c8c77c685391f69acd029a0b77e614e57c890f
Tomek Polański <polanski.tomek@gmail.com>
2020-04-12 09:50:07 +0200

    WIP

--- windows/main.cpp
+++ windows/main.cpp
@@ -1,79 +1,30 @@
-// Copyright 2018 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 #include <flutter/flutter_view_controller.h>
 #include <windows.h>
 
 #include <chrono>
-#include <codecvt>
 #include <iostream>
-#include <string>
 #include <vector>
 
 #include "flutter/generated_plugin_registrant.h"
 #include "win32_window.h"
 #include "window_configuration.h"
 
-namespace {
-
-// Returns the path of the directory containing this executable, or an empty
-// string if the directory cannot be found.
-std::string GetExecutableDirectory() {
-  wchar_t buffer[MAX_PATH];
-  if (GetModuleFileName(nullptr, buffer, MAX_PATH) == 0) {
-    std::cerr << "Couldn't locate executable" << std::endl;
-    return "";
-  }
-  std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> wide_to_utf8;
-  std::string executable_path = wide_to_utf8.to_bytes(buffer);
-  size_t last_separator_position = executable_path.find_last_of('\\');
-  if (last_separator_position == std::string::npos) {
-    std::cerr << "Unabled to find parent directory of " << executable_path
-              << std::endl;
-    return "";
-  }
-  return executable_path.substr(0, last_separator_position);
-}
-
-}  // namespace
-
-int APIENTRY wWinMain(HINSTANCE instance, HINSTANCE prev, wchar_t *command_line,
-                      int show_command) {
+int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
+                      _In_ wchar_t *command_line, _In_ int show_command) {
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
     ::AllocConsole();
   }
 
-  // Resources are located relative to the executable.
-  std::string base_directory = GetExecutableDirectory();
-  if (base_directory.empty()) {
-    base_directory = ".";
-  }
-  std::string data_directory = base_directory + "\\data";
-  std::string assets_path = data_directory + "\\flutter_assets";
-  std::string icu_data_path = data_directory + "\\icudtl.dat";
-
-  // Arguments for the Flutter Engine.
-  std::vector<std::string> arguments;
+  flutter::DartProject project(L"data");
 
   // Top-level window frame.
   Win32Window::Point origin(kFlutterWindowOriginX, kFlutterWindowOriginY);
   Win32Window::Size size(kFlutterWindowWidth, kFlutterWindowHeight);
 
-  flutter::FlutterViewController flutter_controller(
-      icu_data_path, size.width, size.height, assets_path, arguments);
+  flutter::FlutterViewController flutter_controller(size.width, size.height,
+                                                    project);
   RegisterPlugins(&flutter_controller);
 
   // Create a top-level win32 window to host the Flutter view.
@@ -90,7 +41,7 @@ int APIENTRY wWinMain(HINSTANCE instance, HINSTANCE prev, wchar_t *command_line,
   std::chrono::nanoseconds wait_duration(0);
   // Run until the window is closed.
   while (window.GetHandle() != nullptr) {
-    MsgWaitForMultipleObjects(0, NULL, FALSE,
+    MsgWaitForMultipleObjects(0, nullptr, FALSE,
                               static_cast<DWORD>(wait_duration.count() / 1000),
                               QS_ALLINPUT);
     MSG message;
