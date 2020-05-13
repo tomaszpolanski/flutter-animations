ef6a571068523270086fe61d2374361e5916f7d2
Tomek Polański <polanski.tomek@gmail.com>
2020-05-13 21:55:56 +0200

    Using the project for tutorial screenshots

---
+++ lib/pages/frame_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class FramePage extends StatelessWidget {
+  const FramePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: ThemeData.light().copyWith(
+        primaryColor: Colors.green,
+      ),
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Align: without Align')),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Align(
+              child: RaisedButton(
+                onPressed: () {},
+                child: Text(
+                  'Button',
+                  style: GoogleFonts.roboto(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- windows/window_configuration.cpp
+++ windows/window_configuration.cpp
@@ -1,7 +1,7 @@
 #include "window_configuration.h"
 
-const wchar_t *kFlutterWindowTitle = L"Flutter Animations";
+const wchar_t *kFlutterWindowTitle = L"Align";
 const unsigned int kFlutterWindowOriginX = 10;
 const unsigned int kFlutterWindowOriginY = 10;
-const unsigned int kFlutterWindowWidth = 800;
-const unsigned int kFlutterWindowHeight = 800;
+const unsigned int kFlutterWindowWidth = 400;
+const unsigned int kFlutterWindowHeight = 600;
