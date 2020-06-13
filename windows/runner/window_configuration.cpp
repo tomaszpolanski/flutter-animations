4075f55330e25ceba895e4a4d4f42b567dd5ab6d
Tomek Polanski <tpolanski@groupon.com>
2020-06-13 10:13:44 +0200

    Flutter 1.19.0-4.1.pre

---
+++ windows/runner/window_configuration.cpp
@@ -0,0 +1,7 @@
+#include "window_configuration.h"
+
+const wchar_t* kFlutterWindowTitle = L"Flutter Animations";
+const unsigned int kFlutterWindowOriginX = 10;
+const unsigned int kFlutterWindowOriginY = 10;
+const unsigned int kFlutterWindowWidth = 500;
+const unsigned int kFlutterWindowHeight = 900;
--- windows/window_configuration.cpp
+++
@@ -1,7 +0,0 @@
-#include "window_configuration.h"
-
-const wchar_t *kFlutterWindowTitle = L"Align";
-const unsigned int kFlutterWindowOriginX = 10;
-const unsigned int kFlutterWindowOriginY = 10;
-const unsigned int kFlutterWindowWidth = 400;
-const unsigned int kFlutterWindowHeight = 600;
